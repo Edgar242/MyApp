4b2043bf4d92796dfe1c0c6d9b265dd02721d69d
Edgar <edgar.zamora.aranda@gmail.com>
2021-02-14 04:28:48 -0600

    Added Tarea1

---
+++ MyApp/ViewControllerTarea1.swift
@@ -0,0 +1,39 @@
+//
+//  ViewControllerTarea1.swift
+//  MyApp
+//
+//  Created by Edgar on 14/02/21.
+//
+
+import UIKit
+
+class ViewControllerTarea1: UIViewController {
+  @IBAction func onSwitchChangeMood(_ sender: UISwitch) {
+    if switchMood.isOn {
+      print("I am happy");
+      imageViewMood.image = UIImage(named: "smiley");
+    } else {
+      print("I am sad");
+    }
+  }
+  
+  @IBOutlet weak var switchMood: UISwitch!
+  @IBOutlet weak var imageViewMood: UIImageView!
+  override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
