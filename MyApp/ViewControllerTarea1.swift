51654a667d60169d85bd4e3f0ec8cfe7eaa5123a
Edgar <edgar.zamora.aranda@gmail.com>
2021-02-14 19:46:20 -0600

    Added dark mode in chatbox

--- MyApp/ViewControllerTarea1.swift
+++ MyApp/ViewControllerTarea1.swift
@@ -8,23 +8,84 @@
 import UIKit
 
 class ViewControllerTarea1: UIViewController {
-  @IBAction func onSwitchChangeMood(_ sender: UISwitch) {
-    if switchMood.isOn {
-      imageViewMood.image = UIImage(systemName: "play");
+  @IBOutlet weak var switchDarkBackground: UISwitch!
+  @IBOutlet weak var switchMood: UISwitch!
+  @IBOutlet weak var imageViewMood: UIImageView!
+  @IBOutlet weak var sliderVolume: UISlider!
+  @IBOutlet weak var segCtrlVideoPlayer: UISegmentedControl!
+  @IBOutlet weak var buttonAdd: UIButton!
+  @IBOutlet weak var textViewInfo: UITextView!
+  @IBOutlet weak var textFieldPhrase: UITextField!
+  var soundLevel = "";
+  
+  @IBAction func switchOnChangeDarkBackground(_ sender: UISwitch) {
+    if switchDarkBackground.isOn {
+      textViewInfo.backgroundColor = UIColor.black;
+      textViewInfo.textColor = UIColor.white;
+      textFieldPhrase.backgroundColor = UIColor.black;
+      textFieldPhrase.textColor = UIColor.white;
     } else {
-      imageViewMood.image = UIImage(systemName: "stop");
+      textViewInfo.backgroundColor = UIColor.white;
+      textViewInfo.textColor = UIColor.black;
+      textFieldPhrase.backgroundColor = UIColor.white;
+      textFieldPhrase.textColor = UIColor.black;
     }
   }
   
-  @IBOutlet weak var switchMood: UISwitch!
-  @IBOutlet weak var imageViewMood: UIImageView!
+  @IBAction func onChangeButtonAdd(_ sender: UIButton) {
+    textViewInfo.text += textFieldPhrase.text! + "\r\n";
+    textFieldPhrase.text = "";
+  }
+  
+  @IBAction func onChangeVideoPlayer(_ sender: UISegmentedControl) {
+    switch segCtrlVideoPlayer.selectedSegmentIndex {
+    case 0:
+      textViewInfo.text = "Rewind video";
+    case 1:
+      textViewInfo.text = "Play video";
+    case 2:
+      textViewInfo.text = "Pause video";
+    case 3:
+      textViewInfo.text = "Next video";
+    default:
+      print("default");
+    }
+  }
+  
+  
+  @IBAction func sliderVolumeOnChange(_ sender: UISlider) {
+    switch sliderVolume.value {
+    case 0..<1:
+      soundLevel = "speaker";
+    case 1..<2:
+      soundLevel = "speaker.1";
+    case 2..<3:
+      soundLevel = "speaker.2";
+    case 3...4:
+      soundLevel = "speaker.3";
+    default:
+      print(sliderVolume.value)
+    }
+    
+    imageViewMood.image = UIImage(systemName: soundLevel);
+  }
+    
+  
+  @IBAction func onSwitchChangeMood(_ sender: UISwitch) {
+    if switchMood.isOn {
+      imageViewMood.image = UIImage(systemName: "speaker.slash.fill");
+    } else {
+      imageViewMood.image = UIImage(systemName: soundLevel);
+    }
+  }
+  
   override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
-    }
+    // Do any additional setup after loading the view.
     
-
+    }
+  
     /*
     // MARK: - Navigation
 
